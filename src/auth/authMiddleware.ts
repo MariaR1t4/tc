ce4c1bc4edd2913fc0829682b53ac18a7ff84e3e
unknown <biancalimalorenzini@gmail.com>
2023-11-17 23:01:58 -0300

    senhor socorro

--- src/auth/authMiddleware.ts
+++ src/auth/authMiddleware.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 
 export async function authMiddleware(req: Request, res:Response, next:NextFunction){
-    const token = req.query.token || req.headers['x-acess-token'];
+    const token = req.query.token || req.headers['authorization'];
     console.log(token)
     res.json(token)
 
